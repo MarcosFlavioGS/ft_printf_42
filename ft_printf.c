ca0dcbba03033a0c40537cda73cccefed2a488bf
mflavio- <mfghost69@gmail.com>
2022-10-14 01:56:07 +0200

    começando o dia

--- ft_printf.c
+++ ft_printf.c
@@ -6,22 +6,48 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 05:29:47 by coder             #+#    #+#             */
-/*   Updated: 2022/10/12 19:33:39 by coder            ###   ########.fr       */
+/*   Updated: 2022/10/14 01:55:53 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/ft_printf.h"
 
+static void	hex_convert(int c, int x)
+{
+	int	remainder;
+
+	remainder = c;
+	if (x)
+	{
+	while (remainder % 16 != 0)
+	{
+		return ;
+	}
+	}
+	else
+	{
+		return ;
+	}
+}
+
 static void	checker(char c, va_list ap)
 {
-  if (c == 'c')
-    return (ft_putchar_fd(va_arg(ap, int), 1));
-  else if (c == 's')
-    return (ft_putstr_fd(va_arg(ap, char *), 1));
-  else if (c == 'd' || c == 'i')
-    return (ft_putnbr_fd(va_arg(ap, int), 1));
-  else
-    return (ft_putchar_fd(c, 1));
+	if (c == 'c')
+		return (ft_putchar_fd(va_arg(ap, int), 1));
+	else if (c == 's')
+		return (ft_putstr_fd(va_arg(ap, char *), 1));
+	else if (c == 'p')
+		return (não sei ainda);
+	else if (c == 'd' || c == 'i')
+		return (ft_putnbr_fd(va_arg(ap, int), 1));
+	else if (c == 'u')
+		return (não sei ainda);
+	else if (c == 'x')
+		return (hex_convert(va_arg(ap, int), 1));
+	else if (c == 'X')
+		return (hex_convert(va_arg(ap, int), 0));
+	else
+		return (ft_putchar_fd(c, 1));
 }
 
 int	ft_printf(const char *placeholders, ...)
--- headers/ft_printf.h
+++ headers/ft_printf.h
@@ -6,17 +6,16 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 05:28:22 by coder             #+#    #+#             */
-/*   Updated: 2022/10/12 19:07:20 by coder            ###   ########.fr       */
+/*   Updated: 2022/10/14 01:25:31 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
-#include "libft.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
+# define FT_PRINTF_H
+# include "libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
 
 int	ft_printf(const char *placeholders, ...);
 #endif
